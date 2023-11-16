7632f1571e1357822807cd2eb04c020109261d8d
haikalalihakim <148456995+haikalalihakim@users.noreply.github.com>
2023-11-16 21:57:54 +0700

    alpro no 6

---
+++ variabel.java
@@ -0,0 +1,9 @@
+public class Variabel {
+  public static void main(String[] args) {
+  		String namaSaya = "Haikal Ali Hakim";
+        int tahunlahir = 2001;
+        
+    System.out.println("Nama saya:" + namaSaya);
+    System.out.println("Tahun lahir:"+ tahunlahir);
+  }
+} 
